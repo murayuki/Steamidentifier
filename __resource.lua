8d7c9433e8a6ab20701bc9a049ea659061530538
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-11-04 11:12:53 -0300

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+description "Steam identifier by Dislaik"
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
+
+
+server_scripts {
+	'server.lua'
+}
