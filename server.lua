d608ffbe2266abf637089d45ae720aa08cbc5b1f
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-11-04 11:10:34 -0300

    Smallo contribution

--- server.lua
+++ server.lua
@@ -2,21 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-function getIdentity(source)
-	local identifier = GetPlayerIdentifiers(source)[1]
-	local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
-	if result[1] ~= nil then
-		local identity = result[1]
-
-		return {
-			identifier = identity['identifier'],
-		}
-	else
-		return nil
-	end
-end
-
 ESX.RegisterServerCallback('esx_steamid:id', function(source, steam)
-    local id = getIdentity(source)
-    steam(id.identifier)
+    steam(GetPlayerIdentifiers(source)[1])
 end)
