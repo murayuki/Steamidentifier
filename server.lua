052d6b659e4be74d8824991939893f9c958ada83
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2019-11-04 14:47:44 -0300

    Delete server.lua

--- server.lua
+++
@@ -1,7 +0,0 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterServerCallback('esx_steamid:id', function(source, steam)
-    steam(GetPlayerIdentifiers(source)[1])
-end)
